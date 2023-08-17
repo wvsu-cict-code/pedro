3b275dc4f4056792d223cbf1b181fc69342bd5f4
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2023-08-17 08:35:34 +0800

    Other languages support

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -8,6 +8,15 @@ import shuffle from 'fisher-yates';
 import en_data from '../data/en/questions.json';
 import intro_en_data from '../data/en/intro.json'
 import extro_en_data from '../data/en/extro.json'
+
+import ta_data from '../data/ta/questions.json';
+import intro_ta_data from '../data/ta/intro.json'
+import extro_ta_data from '../data/ta/extro.json'
+
+import hi_data from '../data/hi/questions.json';
+import intro_hi_data from '../data/hi/intro.json'
+import extro_hi_data from '../data/hi/extro.json'
+
 import BouncingLoader from "./BouncingLoader";
 import Card from "./Card";
 import ChatBubble from "./ChatBubble";
@@ -23,32 +32,40 @@ const fuseOptions = {
 
 const wait_time = 1500
 
-const fuse = new Fuse(en_data, fuseOptions);
+const fuse_en = new Fuse(en_data, fuseOptions);
+const fuse_ta = new Fuse(ta_data, fuseOptions);
+const fuse_hi = new Fuse(hi_data, fuseOptions);
 
 enum Position {
     Left = 0,
     Right = 1
 }
+
+enum IChatMode {
+    English = 0,
+    Tagalog = 1,
+    Hiligaynon = 2
+}
 interface IConversation {
     position: Position
     message: ReactElement
-    id:Key
+    id: Key
 }
 
 interface IConverstions extends Array<IConversation> { }
 
-export default function Chat() {
+export default function Chat(props: any) {
+
+    const ChatMode: IChatMode = (props.ChatMode)
 
     const [chatbox_text, setChatBoxText] = useState("")
     const [questions_visible, toggleQuestions] = useState(true)
-    const [conversations, setConverstations] = useState<IConverstions>([
-        { id:uuidv4(), position: Position.Left, message: <span>{shuffle(intro_en_data)[0].body}</span> }
-    ])
+    const [conversations, setConverstations] = useState<IConverstions>([])
     const [is_loading, setLoading] = useState(false)
     const [result_count, setResultCount] = useState(6)
-
     useEffect(() => {
         let timer: any = null;
+
         if (is_loading) {
             timer = setInterval(() => {
                 setLoading(false)
@@ -66,34 +83,73 @@ export default function Chat() {
         toggleQuestions(false)
         setChatBoxText(query)
         // query
-        setConverstations(conversations => [...conversations, { position: Position.Right, message: <span>{query}</span>, id:uuidv4() }])
+        setConverstations(conversations => [...conversations, { position: Position.Right, message: <span>{query}</span>, id: uuidv4() }])
     }
 
     function replyMessage(query: string) {
 
-        const result = fuse.search(query)
+        const result_en = fuse_en.search(query)
+        const result_ta = fuse_ta.search(query)
+        const result_hi = fuse_hi.search(query)
+
         const not_found_message: ReactElement = <span><FaExclamationCircle className="text-yellow-500 mb-2" /><span> I found no matches with your query. You can check our </span><a className="underline" href="#" onClick={e => {
             e.preventDefault()
             setResultCount(en_data.length)
             toggleQuestions(true)
         }}>FAQ list</a>.</span>
 
-        if (result.length > 0) {
-            // A score of 0 is perfect match
-            const score = result[0].score ? result[0].score : 1
-            console.log({ result })
-            console.log({ score })
-
-            setConverstations(conversations => [...conversations, { id: result[0].item.id,position: Position.Left, message: <span><TypeAnimation style={{whiteSpace: 'pre-line'}} speed={80} cursor={false} sequence={[result[0].item.response +'\n\n'+ shuffle(extro_en_data)[0].body]} /></span> }])
-        } else {
-            setConverstations(conversations => [...conversations, { id: uuidv4(), position: Position.Left, message: not_found_message }])
+        if (ChatMode == IChatMode.English) {
+            if (result_en.length > 0) {
+                // A score of 0 is perfect match
+                const score = result_en[0].score ? result_en[0].score : 1
+                setConverstations(conversations => [...conversations, { id: result_en[0].item.id, position: Position.Left, message: <span><TypeAnimation style={{ whiteSpace: 'pre-line' }} speed={80} cursor={false} sequence={[(result_en[0].item.response == "RANDOM" ? shuffle(intro_en_data)[0].body : result_en[0].item.response) + '\n\n' + shuffle(extro_en_data)[0].body]} /></span> }])
+            } else {
+                setConverstations(conversations => [...conversations, { id: uuidv4(), position: Position.Left, message: not_found_message }])
+            }
         }
+
+        if (ChatMode == IChatMode.Tagalog) {
+            if (result_ta.length > 0) {
+                // A score of 0 is perfect match
+                const score = result_ta[0].score ? result_ta[0].score : 1
+                setConverstations(conversations => [...conversations, { id: result_en[0].item.id, position: Position.Left, message: <span><TypeAnimation style={{ whiteSpace: 'pre-line' }} speed={80} cursor={false} sequence={[(result_en[0].item.response == "RANDOM" ? shuffle(intro_ta_data)[0].body : result_ta[0].item.response) + '\n\n' + shuffle(extro_ta_data)[0].body]} /></span> }])
+            } else {
+                setConverstations(conversations => [...conversations, { id: uuidv4(), position: Position.Left, message: not_found_message }])
+            }
+        }
+
+        if (ChatMode == IChatMode.Hiligaynon) {
+            if (result_hi.length > 0) {
+                // A score of 0 is perfect match
+                const score = result_hi[0].score ? result_hi[0].score : 1
+                setConverstations(conversations => [...conversations, { id: result_en[0].item.id, position: Position.Left, message: <span><TypeAnimation style={{ whiteSpace: 'pre-line' }} speed={80} cursor={false} sequence={[(result_hi[0].item.response == "RANDOM" ? shuffle(intro_hi_data)[0].body : result_hi[0].item.response) + '\n\n' + shuffle(extro_hi_data)[0].body]} /></span> }])
+            } else {
+                setConverstations(conversations => [...conversations, { id: uuidv4(), position: Position.Left, message: not_found_message }])
+            }
+        }
+
     }
+
+    console.log(props)
 
     return (
         <div>
             {questions_visible && <ul role="list" className="grid md:grid-cols-2 gap-4 sm:grid-cols-1">
-                {en_data.slice(0, result_count).map((question: any) => <Card key={uuidv4()}
+                {ChatMode == IChatMode.English && en_data.slice(0, result_count).map((question: any) => <Card key={uuidv4()}
+                    onClick={() => {
+                        sendMessage(question.body)
+                    }}
+                    tags={question.tags[0]}
+                    body={question.body}
+                />)}
+                {ChatMode == IChatMode.Tagalog && ta_data.slice(0, result_count).map((question: any) => <Card key={uuidv4()}
+                    onClick={() => {
+                        sendMessage(question.body)
+                    }}
+                    tags={question.tags[0]}
+                    body={question.body}
+                />)}
+                {ChatMode == IChatMode.Hiligaynon && hi_data.slice(0, result_count).map((question: any) => <Card key={uuidv4()}
                     onClick={() => {
                         sendMessage(question.body)
                     }}
@@ -102,6 +158,30 @@ export default function Chat() {
                 />)}
             </ul>}
             <div className="mt-4 grid grid-cols-1 gap-4">
+                {
+                    ChatMode == IChatMode.English && <ChatBubble
+                        key={"intro"}
+                        id={uuidv4()}
+                        position={Position.Left}
+                        message={<span>{shuffle(intro_en_data)[0].body}</span>}
+                    />                    
+                }
+                {
+                    ChatMode == IChatMode.Tagalog && <ChatBubble
+                    key={"intro"}
+                    id={uuidv4()}
+                    position={Position.Left}
+                    message={<span>{shuffle(intro_ta_data)[0].body}</span>}
+                />
+                }
+                {
+                    ChatMode == IChatMode.Hiligaynon && <ChatBubble
+                    key={"intro"}
+                    id={uuidv4()}
+                    position={Position.Left}
+                    message={<span>{shuffle(intro_hi_data)[0].body}</span>}
+                />
+                }
                 {conversations.map(i => (
                     <ChatBubble
                         key={i.id}
@@ -113,6 +193,12 @@ export default function Chat() {
                 {is_loading && <ChatBubble id={uuidv4()} key={uuidv4()} position={Position.Left} message={<BouncingLoader />} />}
             </div>
             <div className="mt-6">
+                <div className='mb-2'>
+                    <span>Switch to:</span>
+                    {ChatMode == IChatMode.English ? null : <a className='bg-green-800 text-white rounded hover:bg-green-700 px-4 py-2 text-center mx-2' href="/en">English</a>}
+                    {ChatMode == IChatMode.Tagalog ? null : <a className='bg-green-800 text-white rounded hover:bg-green-700 px-4 py-2 text-center mx-2' href="/ta">Tagalog</a>}
+                    {ChatMode == IChatMode.Hiligaynon ? null : <a className='bg-green-800 text-white rounded hover:bg-green-700 px-4 py-2 text-center mx-2' href="/hi">Hiligaynon</a>}
+                </div>
                 <div className="grid md:grid-cols-6 sm:grid-cols-1 gap-4">
                     <div className="md:col-span-5">
                         <textarea value={chatbox_text} onChange={({ target }) => {
